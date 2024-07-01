6ef0925643f7a597ee3656a290fb7d634c0ff290
maplonki <hmurillo1891@gmail.com>
2024-07-01 14:06:23 -0700

    Add boilerplate code

--- app/src/main/java/com/example/otchallenge/MainActivity.kt
+++ app/src/main/java/com/example/otchallenge/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
 	override fun onCreate(savedInstanceState: Bundle?) {
+		(application as MyApplication).appComponent.inject(this)
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
