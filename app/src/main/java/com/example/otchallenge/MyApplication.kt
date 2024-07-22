7f4cdec8382ce78626a3dcd2a0f32420b4cbddd5
ErickMartinez <eoma_martinez@hotmail.com>
2024-07-22 00:29:19 -0600

    Provide App dependencies using AppComponent

--- app/src/main/java/com/example/otchallenge/MyApplication.kt
+++ app/src/main/java/com/example/otchallenge/MyApplication.kt
@@ -3,6 +3,7 @@ package com.example.otchallenge
 import android.app.Application
 import com.example.otchallenge.di.AppComponent
 import com.example.otchallenge.di.DaggerAppComponent
+import timber.log.Timber
 
 class MyApplication : Application() {
 
@@ -10,6 +11,21 @@ class MyApplication : Application() {
 
 	override fun onCreate() {
 		super.onCreate()
-		appComponent = DaggerAppComponent.builder().build()
+		appComponent = DaggerAppComponent
+			.builder()
+			.baseUrl("https://api.nytimes.com/")
+			.context(this)
+			.nyTimesApiKey(BuildConfig.nyTimesApiKey)
+			.build()
+
+		setupTimber()
+	}
+
+	private fun setupTimber() {
+		if (BuildConfig.DEBUG) {
+			Timber.plant(Timber.DebugTree())
+		} else {
+			//plant production analytics
+		}
 	}
 }
--- app/src/main/java/com/example/otchallenge/di/AppComponent.kt
+++ app/src/main/java/com/example/otchallenge/di/AppComponent.kt
@@ -1,11 +1,84 @@
 package com.example.otchallenge.di
 
-import com.example.otchallenge.MainActivity
+import android.content.Context
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.care.sdk.utils.StringResourceProxy
+import com.care.sdk.utils.StringResources
+import com.example.otchallenge.Database
+import dagger.Binds
+import dagger.BindsInstance
 import dagger.Component
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.Locale
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(modules = [ApplicationModule::class])
 interface AppComponent {
-	fun inject(activity: MainActivity)
+
+	@NYTimesApiKey
+	fun nyTimesApiKey(): String
+
+	@BaseUrl
+	fun baseUrl(): String
+
+	fun retrofit(): Retrofit
+
+	fun locale(): Locale
+
+	fun glide(): RequestManager
+
+	fun stringResources(): StringResources
+
+	fun sqlDatabase(): Database
+
+	@Component.Builder
+	interface Builder {
+		@BindsInstance
+		fun baseUrl(@BaseUrl baseUrl: String): Builder
+
+		@BindsInstance
+		fun context(context: Context): Builder
+
+		@BindsInstance
+		fun nyTimesApiKey(@NYTimesApiKey apiKey: String): Builder
+
+		fun build(): AppComponent
+	}
+}
+
+@Module
+interface ApplicationModule {
+
+	@Binds
+	fun bindsStringResource(stringResources: StringResourceProxy): StringResources
+
+	@Module
+	companion object {
+		@Provides
+		fun providesRetrofit(@BaseUrl baseUrl: String): Retrofit {
+			return Retrofit.Builder()
+				.baseUrl(baseUrl)
+				.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+				.addConverterFactory(MoshiConverterFactory.create())
+				.build()
+		}
+
+		@Provides
+		fun providesLocale(): Locale = Locale.getDefault()
+
+		@Provides
+		fun providesGlide(context: Context) = Glide.with(context)
+
+		@Provides
+		fun provideSQLDatabase(context: Context): Database = Database(
+			AndroidSqliteDriver(Database.Schema, context, "books.db")
+		)
+	}
 }
